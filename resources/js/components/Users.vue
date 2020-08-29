ee422bc590b75f363596736c54c3f93559a98d05
vndrd <ivangddzzz@gmail.com>
2020-08-29 18:00:38 -0400

    display users

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -22,15 +22,17 @@
                                     <th>Name</th>
                                     <th>Email</th>
                                     <th>Type</th>
+                                    <th>Registered At</th>
                                     <th>Modify</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-7-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
+                                <tr v-for="user in users" :key="user.id">
+                                    <td>{{user.id}}</td>
+                                    <td>{{user.name}}</td>
+                                    <td>{{user.email}}</td>
+                                    <td>{{user.type}}</td>
+                                    <td>{{user.created_at}}</td>
                                     <td>
                                         <a href="#">
                                             <i class="fa fa-edit blue"></i>
@@ -116,6 +118,7 @@
     export default {
         data(){
             return{
+                users: [],
                 form: new Form({
                     name: '',
                     email: '',
@@ -127,12 +130,16 @@
             }
         },
         mounted() {
-            console.log('Component mounted.USERS' )
+            this.loadUsers()
         },
         methods: {
             createUser(){
-                console.log('creating user')
                 this.form.post('api/user')
+            },
+            loadUsers(){
+                axios.get("api/user").then( ({data}) => {
+                    this.users = data.data
+                })
             }
         }
     }
